fa564c07f3b7657020bc410bc5e9a6ffea5ab1e1
suhao1 <suhao1@vipkid.com.cn>
2018-10-10 02:07:00 +0800

    静态样式

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sundries-box
+# font 标签不识别，改为 p
+# img不能定义大小，<img 批量替换为 <img class="pic"
+# 批量删除&#8203;
---
+++ src/utils/atcDetail.js
@@ -0,0 +1,80 @@
+let atc = `<div class="main_editor " node-type="articleContent">
+                                                        <div class="title" node-type="articleTitle">包子的减肥分享 第一更</div>
+                    <div class="authorinfo clearfix S_txt2">
+                      <div class="W_fl">
+                                              <span class="author1 W_autocut"><img class="pic" title="" alt="" src="//tvax3.sinaimg.cn/crop.0.0.996.996.50/64ba8b53ly8fsu47b1zs5j20ro0ro76h.jpg" class="W_face_radius"><a href="/u/1689946963" class="S_txt1" target="_blank"><em class="W_autocut">包子SAMA圆滚滚</em>
+                                                        <i title="微博个人认证 " class="W_icon icon_approve"></i>
+                                                    </a></span>
+                            <em class="W_vline S_line1"></em>
+                                             
+                        
+                                                <span class="time"> 发布于 2017-03-23 19:19:30</span>
+                        
+
+                                                      <em class="W_vline S_line1"></em>
+                            <span class="del"><a href="javascript:void(0);" onclick="javascript:window.open('http://service.account.weibo.com/reportspam?rid=4088542562871980&amp;type=32&amp;from=11101', 'newwindow', 'height=700, width=550, toolbar =yes, menubar=no, scrollbars=yes, resizable=yes, location=no, status=no');" suda-uatrack="key=tblog_headline_article&amp;value=headline_pc_trend_report_click">举报</a></span>
+                          
+                                                                            
+                      </div>
+                                             <div class="W_fr"><span class="num">阅读数：14万+</span></div>
+                                            </div>
+                                                            <div class="preface">在总结减肥的经验和计划之前，包子还是要首先说一下最基本的误区。</div>
+                    
+                                                        <div class="WB_editor_iframe" node-type="contentBody" style="opacity: 1; zoom: 1; user-select: none;" unselectable="on">
+                          
+                          <p>在开始真正的减肥计划的制定之前，有很多有助于减肥计划的知识需要具体说明。</p><p><b>首先要重点说明的就是肌肉的重要性，很多小伙伴其实对肌肉的重要性都已经有了不少了解，但是还是看到有些小伙伴说想要减掉自己身上的肌肉。所以包子还是坚持反复的强调这个问题，通过减肥来达到更好的身材核心的问题就在于肌肉的保持。</b></p><p>训练也好，饮食的调整也好，最终的目的大部分都是为了维持肌肉的总量同时减少脂肪。否则即使减掉了100斤的体重，也不能加入好身材的行列。</p><p>很多人被“瘦就是好”身材的幻觉笼罩着，但是这种说法并不正确。</p><p>胖对于外形和健康都的影响都是不好的，但是瘦不一定是好身材。</p><p>以一位美国的大码模特Rosie Mercado来举例</p><p><br></p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx3.sinaimg.cn/large/64ba8b53ly1fdwz588ft1j20f80ah4au.jpg"></p><p>​Rosie Mercado身高168cm,直观的感觉大概在140-150斤之间。但是事实上她的真实体重是180斤。</p><p>日常生活中我们也会经常遇到在相同身高和体重下身材完全不同的情况，原因就在于脂肪和肌肉的比例。</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx3.sinaimg.cn/large/64ba8b53ly1fdwz6kmzosj20bg083gqs.jpg" alt="这是YouTube上一个实验视频的截图，5磅的脂肪和5磅的肌肉 。
+​"><span class="picinfo">这是YouTube上一个实验视频的截图，5磅的脂肪和5磅的肌肉 。
+​</span></p><p>​</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx2.sinaimg.cn/large/64ba8b53ly1fdwz74ndqej208l089791.jpg"></p><p>​同样的65KG但是在前后的体脂之下身材对比还是非常强烈的。但是体脂9%就是吹牛了，在自然状态下人类女性是不可能拥有低于12%的体脂率的（实际上我国很多女性15%甚至18%都是某种极限）。</p><p>人类遗传下来的潜意识决定了，原则上“自然生长的肌肉越多，以正常认知的身材标准来看，身材会越好。”</p><p>肌肉的线条能够弥补骨架的先天不足，很多人以为的“溜肩膀”其实只是三角肌薄弱，而不是因为斜方肌粗大；小腿由于脂肪过厚且分布不均，内部的肌肉才会顶出一块来，误认为是“肌肉腿”；背部肌肉过于薄弱认为是前交叉综合征等情况，一切问题的源头还是脂肪过高，而不是肌肉的锅。</p><p>永远不用担心肌肉的过分增长，这也是很多女性都非常担心的问题，成年人类的增肌极限是0.23KG/月，有很多人表示自己远超了这个量，只能说明多出来的重量是储存在肌肉里的糖原和它所附着的水分而不是肌肉纤维（或者你使用了类固醇）。</p><p>如果只是追求瘦而没有肌肉的话，体型大概就是鲁豫姐姐和如下图的状态。</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx2.sinaimg.cn/large/64ba8b53ly1fdwz7wxccaj206609pdk3.jpg"></p><p><b>​大多数人应该都不愿意变成这样的体型。</b></p><p><b>再比如某减肥节目中的某位减肥非常“成功”的选手，虽然减掉了很多体重，但是身材走样严重。我们虽然肯定她非常的努力，但是由于走错了方向，很难再重新增长肌肉，错过了达到最佳体型的最好时机。</b></p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx1.sinaimg.cn/large/64ba8b53ly1fdwz938cm0j204q08y0vj.jpg"></p><p>​体脂率的参考如下图</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx1.sinaimg.cn/large/64ba8b53ly1fdwz9p9b98j207309ijwz.jpg"></p><p>​这是脂肪比例对应的身材大致图样，虽然只是普通层面的参考图，但是也能让大家明白基本情况。</p><p>也就是说即使是180斤的体重，18%的体脂也会拥有维密天使一样的好身材。</p><p>（但是这是不可能的，因为FFMI极限限制，后面会具体解释）</p><p>即使80斤重，体脂40%，也是一个毫无身材可言的骷髅（同样是不可能的，后面具体解释+1）。</p><p>女性体脂18%~22%，男性体脂15%，是人类体型最好看的比例。</p><p>就是同体脂率下，肌肉和脂肪的总量越高（这与红白肌比例有关，之后更新会有论述），身材越好。</p><p>同样体脂22%，150斤肯定要比100斤身材性感的多。如下图：</p><p>图一、高脂肪高肌肉总量体脂22%</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx4.sinaimg.cn/large/64ba8b53ly1fdwzae196hj206e06f41o.jpg"></p><p>​图二、低脂肪低级肉总量体脂22%</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx2.sinaimg.cn/large/64ba8b53ly1fdwzatvjs1j2081056n07.jpg"></p><p>​一般人都会觉得图一更加性感。当然图二也不错。</p><p>但是体脂含量也不是一味的越低越好，这是有极限的。当女性体脂降低到12%左右的时候，身材是非常不符合大众审美的，但是在不使用类固醇的情况下也是不可能发生的。</p><p>为什么呢？</p><p><b>这里要说一个重要的概念。FFMI指数</b></p><p>FFMI指数是指人类去脂肪体重指数。</p><p>这个指数常被用来判断一个人是否使用了类固醇药物和判断一个人的身材潜力。</p><p>FFMI的公式是：</p><p><p style="color: #d24a35">FFMI = [体重（千克单位）*(100%-体脂率)] / 身高（米）的平方</p></p><p>身高高于1米80厘米，每10厘米数值再加上0.6</p><p>FFMI指数的男性极限是26，女性极限是24.5。（在低体脂的情况下，这个数值越接近极限，理论身材越好）</p><p>超过这个数字的人，无疑是使用了类固醇药物，无论他用任何借口辩解，例如努力、天赋、基因、甚至召唤龙珠，毫无疑问，他都肯定使用了类固醇药物，这是无法辩驳的。</p><p>而且，这个极限数字是理论上的，实际应用中无论你营养多么充足，训练多么不要命，你也不可能达到这个指数，历史上“距离自然健美极限最近的人”——弗兰克·赞恩，他的FFMI指数是25.3，如下图：</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx1.sinaimg.cn/large/64ba8b53ly1fdwzbhc1szj205r07n77z.jpg"></p><p>​这已经是人类在自然状态下所能达到的极限了（但是赞恩本人还是使用了类固醇）。</p><p>有人可能会说，不对，我见过更壮的，施瓦辛格呢？黑猩猩那种的呢？老美不是到处都是肌肉男吗？</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx2.sinaimg.cn/large/64ba8b53ly1fdwzbvw3p5j204t04y3z1.jpg"></p><p>​</p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx4.sinaimg.cn/large/64ba8b53ly1fdwzc6pdluj205f09gtb7.jpg"></p><p>​大卡特，FFMI超过30，健美人群以外的人一般都比较难接受这样的身材，他无疑使用了禁药。</p><p>或者下图，州长大人，FFMI28.7，他亲口承认从16岁就使用了第一代类固醇大力补，并且代价是做了两次心脏大手术。</p><p><br>​​​​</p><fieldset class="line"><legend>专属会员可免费查看以下内容</legend></fieldset><p><br></p><p img-box="img-box" class="picbox"><img class="pic" src="https://wx4.sinaimg.cn/large/64ba8b53ly1fdwzd32eo2j205y07n781.jpg"></p><p>​就是说，这种肌肉过度的人，并不是自然生长的结果，他们使用了特殊药物。</p><p><br></p><p><b>很多女性提起肌肉就害怕，生怕自己随便练练就成了韩国的金刚芭比。</b></p><p>但是遗憾的是，那些都是突破人类极限的身材了，人类不依靠禁药是不能达到那种肌肉水平的（或者注射了西斯龙，一种类似隆胸硅胶原理的假肌肉）</p><p>多说一句，现在类固醇已经成了各行各业通用的超人药剂，它可以超快速增长肌肉，提高身体稳定性，好处诸多。健身模特、演员、歌手、运动员、特殊行业者、音乐家……但是类固醇的副作用是巨大的，这会在以后的更新中提到。</p><p><b>我们反过来讲刚才说到的一种情况，一位女性，身高170，体脂18%，180斤，有没有可能呢？</b></p><p>计算一下FFMI就知道，如果这样算，她的指数达到了25.5左右，这甚至超过了赞恩，也超过了女性实际上的极限23.5左右。</p><p>如果体脂18%，这已经接近女性自然生长的体脂极限，身高170的女性极限体重是是165斤。</p><p><b>也就是说，人类自然生长的极限，是165斤拥有魔鬼身材。</b></p><p>这还是170身材高挑的妹子，如果是165的，160的，甚至155的，这个数字会大幅下滑。</p><p>这就是为什么你看到的大多数身材极好的妹子，体重都是120斤，110斤，甚至更低的原因（维密的所有天使，体重都超过了100斤）。</p><p>结论就是，如果想要拥有完美的身材，第一点就是体脂要低。自然生长的前提下如果先天骨架条件好（当然这种情况比较罕见）只靠瘦也可以凸显出完美的骨架和比例，除了上述的个例，一般人群必须用肌肉撑起弱势部位，才可以达到完美身材。所以我们要拥有极致身材，需要的不只是瘦，而是只减去脂肪，降低体脂，同时最大限度增长肌肉（实际上女性能做到保持就很好了）。如果减掉了肌肉是很难再长回来的。</p><p>只有了解了这个，我们才能用正确的眼光是分析每一种减肥方法是否合理，以及如何正确挑选适合自己的减肥方案。</p><p>但是没有任何一个男性，体脂可以压到5%以下，医生会告诉你这只能是一具尸体的体脂含量；同样也没有任何一个女性，自然锻炼能达到12%以下的体脂率。</p><p>健身房的体脂测试仪和你买回家的体重秤，研究认为最大误差是8%（但是笔者也见过误差超过15%以上的情况），并且体脂越低或越高，误差越大。这和算卦差不多，只能做个参考。</p><p><b>较低体脂区间，女性判断体脂的简单的方式是看腹肌，例如：</b></p><p><p style="color: #d24a35">14%~16% 背肌显露，腹肌分块更加明显。</p></p><p><p style="color: #d24a35">17%~19%全身各部位脂肪不松弛，腹肌分块明显。</p></p><p><p style="color: #d24a35">20%~22%全身各部位脂肪不松弛，腹肌开始显露，分块不明显。</p></p><p><p style="color: #d24a35">23%~25%全身各部位脂肪基本不松弛，腹肌不显露。</p></p><p><p style="color: #d24a35">26%~28% 全身各部位脂肪就腰腹部明显松弛，腹肌不显露。</p></p><p><p style="color: #d24a35">（17%~25%为女子的理想型体脂率）</p></p><p><b>接下来是男性的：</b></p><p><p style="color: #d24a35">10%~12%全身各部位脂肪不松弛，腹肌分块明显。</p></p><p><p style="color: #d24a35">13%~15%全身各部位脂肪基本不松弛，腹肌开始显露，分块不明显。</p></p><p><p style="color: #d24a35">16%~18%全身各部位脂肪就腰腹部较松弛，腹肌不显露。</p></p><p><p style="color: #d24a35">19%~21% 腹肌不显露，腰围通常是81~85厘米。</p></p><p><p style="color: #d24a35">22%~24% 腹肌不显露，腰围通常是86~90厘米。</p></p><p><p style="color: #d24a35">（10%~18%为男子的理想型体脂率）</p></p><h2>最后包子来回答一些小伙伴们提出的比较多的问题</h2><p><b>1.如何局部瘦身</b></p><p><p style="color: #d24a35">局部瘦身的方法是不存在的，虽然肌肉的应激反应有可能在短时间内适当改变形状，但是这不是真正瘦身，几秒钟后即会回复原状。2013年研究者进行过一次实验，11名男女被安排进行三个月的单腿训练，完成960~1200次腿举，试验结束后发现，两腿没有脂肪量没有任何差别。Refine Method的创始人运动专家Brynn Jinnett称：“锻炼局部肌肉并不能对这块肌肉周围的脂肪有特殊刺激，人类无法决定先减掉哪些脂肪。”针对网球运动员的一份长期观察报告显示，职业运动员惯用手和非惯用手的脂肪没有任何区别……无数例证可以证明，局部减脂是根本不存在的。</p></p><p><p style="color: #d24a35">某些人局部脂肪过多，比如脸上肉肉很多，更多的是基因决定的，即使抽脂也会优先重新长回脸部。唯一的解决方案是首先全身减脂，让全身所有部位脂肪等比例减少（脂肪减少的先后多寡顺序也是基因决定的，一般是最后掉大腿内侧和腹部），直到达到理想局部体脂后，定点锻炼希望增长部位的肌肉，让自己增长或冲碳进行塑性。肌肉可定点生长，脂肪不可定点消除。</p></p><p><b>2.减掉大量体重之后，会不会皮肤松弛？皮肤表面被皮下脂肪撑开的纹路能否消除？</b></p><p><p style="color: #d24a35">如果是节食减肥，皮肤会松弛严重。很多减肥节目的参赛者最后腹部都有相当恐怖的皮肤松弛症状，明显是因为节食过度。高蛋白饮食和适当力量训练可以最大程度的减少这种松弛症。PO主和熊先生的全身皮肤完全没有任何松弛表现，只是腹部皮肤很软，这已经是最大程度的避免皮肤松弛症的结果。皮肤表面被撑开的纹路会随着时间推移颜色慢慢和周围皮肤接近，不仔细观察是比较不容易发现的，但是没有任何完全消除的方式。</p></p><p><b>3.我是肌肉型肥胖，如何减掉肌肉腿？</b></p><p><p style="color: #d24a35">没有所谓肌肉型肥胖，除非使用类固醇，否则肌肉量越大身材会越好。很多女性所谓的肌肉腿无非是三点可能性——</p></p><p><p style="color: #d24a35">1}腿部脂肪过多，堆积的脂肪把腿部填充得十分紧绷，小腿肌肉稍微用力就把肥肉都鼓出来了。接近100%的喊肌肉腿的实际上是这种情况。</p></p><p><p style="color: #d24a35">2}由于长跑过多，把白肌消耗光了。白肌是身材好坏决定性部分，在长距离有氧运动中会分解供能给红肌使用，而剩下的红肌线条并不优美，马拉松运动员因为白肌都被红肌代谢了，没有一个腿型好看的。你需要加大力量训练练出白肌，断掉你的长跑计划。白肌红肌问题会在以后专门更新。</p></p><p><p style="color: #d24a35">3}类固醇使用过度。</p></p><p><b>4.什么是负卡路里食物？</b></p><p><p style="color: #d24a35">不存在负卡路里食物，任何食物都有卡路里，只不过是多寡。包括黄瓜、番茄这种很多人认为是负卡路里的食物也会有卡路里。但是很多食物你即使吃得非常多，不会让你发胖，因为卡路里实在是太少了。这些食物以绿叶型和含水量较高的蔬菜为主。那是不是吃这些就能减肥了？当然是不能的。因为他们的蛋白含量过低，无法支撑肌肉代谢。肌肉是好身材的命脉。</p></p><p><b>5.只吃水果就可以减肥吗？</b></p><p><p style="color: #d24a35">水果内含有大量单糖（碳水化合物），而缺少蛋白。PO主反复说明了，好身材最基本的前提就是保证肌肉不减少的前提下减少脂肪。人体供能分别是碳水（糖）、脂肪、蛋白，如果只补充碳水，不补充蛋白摄入，肌肉就会降解为氨基酸进行糖异生供能。结果是1.肌肉减少身材变差 2.肌肉中富含的水份流失 3.肌肉中糖原减少。这些结果会造成体重短时间大幅下降，但是代价却是身材的毁坏，减了10斤会发现身材越发难看。而且肌肉增长远比大家想象的缓慢得多的多，减掉的肌肉再想涨回来难如登天，体重如果这时候再增加，更多的是纯脂肪。</p><br></p><p><b>6.包子你什么时候更具体的减肥方法</b></p><p><p style="color: #d24a35">具体的减肥方法涉及方方面面，比较复杂，希望能把理论更详细的说明，让大家都能清楚的知道自己应该如何做为什么要这么做。而不是像一些玄学减肥照葫芦画瓢就可以了。而且包子看到了一些小伙伴关于肌肉的误区比较大，所以先写这一篇来纠正这种观念才能更好的实现改变身材的目的</p>。</p><p><br></p><p><br></p><p><b><br></b></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p>​</p>                          <div class="DCI_v2 clearfix">
+                              
+                                                        </div>
+                          </div>
+
+                          <!--v+付费阅读和单篇付费-->
+                          <div node-type="fanService">
+
+                              
+                              <div action-type="reward"></div>
+                          </div>
+
+                                                                         <div class="artical_add_box S_bg2" node-type="addBox">
+                   <div class="btn_line W_tc W_f14 W_fb" node-type="maskContent" style="display:none"><a href="javascript:" action-type="maskFollow" action-data="uid=1689946963&amp;owner_id=1689946963&amp;refer_flag=2309400003_2309404088542560153477">关注作者，阅读全文<i class="W_ficon ficon_arrow_down">c</i></a>
+                   <div class="tips">还有50%的精彩内容，作者设置为仅对粉丝可见</div>
+                   </div>
+                    <div class="PCD_counter_b">
+                      <div class="WB_innerwrap">
+                        <div class="userbox S_line2">
+                          <div class="midbox clearfix">
+                            <div class="pic_wrap">
+                              <p class="pic_box">
+                                <a href="/u/1689946963" target="_blank"><img class="pic" src="//tvax3.sinaimg.cn/crop.0.0.996.996.50/64ba8b53ly8fsu47b1zs5j20ro0ro76h.jpg" width="50" height="50" class="pic"></a>
+                              </p>
+                            </div>
+                            <div class="info_wrap">
+                              <div class="title W_fb W_autocut ">
+                                <a href="/u/1689946963" target="_blank" class="S_txt1">包子SAMA圆滚滚</a>
+                                                        <i title="微博个人认证 " class="W_icon icon_approve"></i>
+                                                                                      </div>
+                              <div class="text S_txt2">
+                                  <a href="/u/1689946963" target="_blank" class="S_txt1">专业减肥科普100年~(人´∀｀)．☆．。．:*･</a>
+                              </div>
+                            </div>
+                            <div class="opt">
+                              <p class="btn_bed">
+                                                                 <a class="W_btn_b W_btn_b_disable" href="javascript:"><em class="W_ficon ficon_right">Y</em>已关注</a>
+                                                            </p></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                 </div>
+                          
+                                                            
+                          <!--因为推荐阅读影响打赏放在作者模块下方-->
+                          <div node-type="fanService">
+                                                                <div extendparam="type=pagecard&amp;version=1d66b78bbcdb1135" rewardcomponentbak="seller=1689946963&amp;bid=1000207805&amp;oid=1022%3A2309404088542560153477&amp;access_type=pcLayer&amp;share=1&amp;sign=4f6649d28f67f937c1fc2e366522aa78"><!--renderData start--><div class="rewardcomponent feed_app_btn_a fend_app_reward"><p class="feed_app_reward_title" node-type="rewardTxt"></p><p class="btn fend_app_reward_btn"><a esuda="key=tblog_weibo_reward&amp;value=reward_button_pc&amp;ext=3525084425:1000207805:article:1022_2309404088542560153477" href="javascript:void(0);" class="btn_b" action-type="buyWrap" action-data="type=reward" node-data="seller=1689946963&amp;buyer=3525084425&amp;object_id=1022:2309404088542560153477&amp;bid=1000207805&amp;share=1&amp;type=5&amp;tid=&amp;from=3&amp;from=1&amp;price=9.90" config-data="reward=9.90">赏</a></p><p class="con S_txt2 fend_app_reward_persons_number"></p> </div><!--renderData end--></div>
+                              
+                              <div action-type="reward"></div>
+                          </div>
+
+                  </div>`
+export default atc
